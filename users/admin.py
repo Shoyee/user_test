2fa931ebeb24f7434d7451c395bc651de48d0071
shoyee <shuoyu_2008@163.com>
2018-07-26 09:10:59 +0800

    add list_display of the user_model into admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import User
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'phone', 'date_joined']
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
